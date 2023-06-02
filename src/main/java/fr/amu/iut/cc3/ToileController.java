8d3958e3b5b05618618fc9d74375729d3fb5479b
mathiasBamasb22008671 <mathias.bamas1@gmail.com>
2023-06-02 16:08:21 +0200

    Ajout tracer et vider + point bind avec textfield

--- src/main/java/fr/amu/iut/cc3/ToileController.java
+++ src/main/java/fr/amu/iut/cc3/ToileController.java
@@ -1,10 +1,12 @@
 package fr.amu.iut.cc3;
 
 import javafx.beans.binding.Bindings;
-import javafx.beans.property.DoubleProperty;
-import javafx.beans.property.SimpleDoubleProperty;
-import javafx.beans.property.SimpleListProperty;
+import javafx.beans.binding.BooleanBinding;
+import javafx.beans.binding.DoubleBinding;
+import javafx.beans.binding.IntegerBinding;
+import javafx.beans.property.*;
 import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -14,13 +16,16 @@ import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -31,9 +36,108 @@ public class ToileController implements Initializable {
     private static int angleDepart = 90;
     private static int noteMaximale = 20;
 
+    @FXML
+    private Pane toile;
+
+    @FXML
+    private GridPane noteGridPane;
+
+    @FXML
+    private TextField comp1;
+
+    @FXML
+    private TextField comp2;
+
+    @FXML
+    private TextField comp3;
+
+    @FXML
+    private TextField comp4;
+
+    @FXML
+    private TextField comp5;
+
+    @FXML
+    private TextField comp6;
+
+    @FXML
+    private VBox errorBox;
+
+    private ChangeListener<Number> pointChange;
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        ArrayList<TextField> listeCompTF = new ArrayList<>(Arrays.asList(comp1, comp2, comp3, comp4, comp5, comp6));
+
+        pointChange = new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number aDouble, Number t1) {
+                for(Circle point : getListePoint()){
+                    if(!point.isVisible()) return;
+                }
+                //resetLigne();
+                tracerLigne();
+            }
+        };
+
+        for(int i = 0; i < listeCompTF.size(); ++i){
+            creationPoint(listeCompTF.get(i), i);
+        }
+    }
+
+    public void creationPoint(TextField textField, int numTF){
+        Circle circle = new Circle(5);
+        IntegerProperty noteProperty = new SimpleIntegerProperty(0);
+
+        BooleanBinding noteValide = new BooleanBinding() {
+            {
+                this.bind(textField.textProperty(), noteProperty);
+            }
+            @Override
+            protected boolean computeValue() {
+                try{
+                    int note = Integer.parseInt(textField.getText());
+                    if(note > 20){
+                        errorBox.setVisible(true);
+                        throw new NumberFormatException();
+                    }else
+                        errorBox.setVisible(false);
+                    noteProperty.setValue(note);
+                    return true;
+                }catch (NumberFormatException e){
+                    return false;
+                }
+            }
+        };
+
+        IntegerBinding xCenter = new IntegerBinding() {
+            {
+                this.bind(noteProperty);
+            }
+            @Override
+            protected int computeValue() {
+                return getXRadarChart(noteProperty.doubleValue(), numTF);
+            }
+        };
+
+        IntegerBinding yCenter = new IntegerBinding() {
+            {
+                this.bind(noteProperty);
+            }
+            @Override
+            protected int computeValue() {
+                return getYRadarChart(noteProperty.doubleValue(), numTF);
+            }
+        };
+
+        circle.centerXProperty().bind(xCenter);
+        circle.centerYProperty().bind(yCenter);
+        circle.visibleProperty().bind(noteValide);
+        circle.centerYProperty().addListener(pointChange);
+        circle.centerXProperty().addListener(pointChange);
+        toile.getChildren().add(circle);
+
     }
 
     int getXRadarChart(double value, int axe ){
@@ -44,6 +148,41 @@ public class ToileController implements Initializable {
     int getYRadarChart(double value, int axe ){
         return (int) (rayonCercleExterieur - Math.sin(Math.toRadians(angleDepart - (axe-1)  * angleEnDegre)) * rayonCercleExterieur
                 *  (value / noteMaximale));
+    }
+
+    public void resetGraph(){
+        for(Node node : noteGridPane.getChildren()){
+            if(node instanceof TextField){
+                ((TextField) node).setText("");
+            }
+        }
+        resetLigne();
+        errorBox.setVisible(false);
+    }
+
+    public ArrayList<Circle> getListePoint(){
+        ArrayList<Circle> listePoints = new ArrayList<>();
+        for(Node node : toile.getChildren()){
+            if(node instanceof Circle) listePoints.add((Circle) node);
+        }
+        return listePoints;
+    }
+
+    public void resetLigne(){
+        toile.getChildren();
+    }
+
+    public void tracerLigne(){
+        ArrayList<Circle> listePoint = getListePoint();
+        for(int i = 0; i < listePoint.size(); i++){
+            Line line = new Line();
+            line.setStartX(listePoint.get(i).getCenterX());
+            line.setStartY(listePoint.get(i).getCenterY());
+            line.setEndX(listePoint.get((i+1)%listePoint.size()).getCenterX());
+            line.setEndY(listePoint.get((i+1)%listePoint.size()).getCenterY());
+            line.setStyle("-fx-stroke: black");
+            toile.getChildren().add(line);
+        }
     }
 
 }
